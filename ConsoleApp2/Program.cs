2ab589d756d525198d6f5f6ed2343792d8b40ed7
kaair <kaairo.gm@gmail.com>
2023-12-06 22:48:14 +0100

    Dodaj pliki projektów.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,86 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+public class Entry
+{
+    public int Hours { get; set; }
+    public int Minutes { get; set; }
+}
+
+class Program
+{
+    static void Main()
+    {
+        List<List<Entry>> pages = new List<List<Entry>>();
+        List<Entry> currentPage = new List<Entry>();
+        int currentPageIndex = 0;
+
+        while (true)
+        {
+            Console.Write("Podaj czas pracy maszyny (godziny i minuty oddzielone spacją): ");
+            string input = Console.ReadLine();
+            string[] inputParts = input.Split(' ');
+
+            if (inputParts.Length != 2 ||
+                !int.TryParse(inputParts[0], out int hours) ||
+                !int.TryParse(inputParts[1], out int minutes) ||
+                hours < 0 || minutes < 0 || minutes >= 60)
+            {
+                Console.WriteLine("Błąd: Nieprawidłowy czas pracy.");
+                continue;
+            }
+
+            Entry entry = new Entry { Hours = hours, Minutes = minutes };
+            currentPage.Add(entry);
+
+            if (currentPage.Count == 3)
+            {
+                pages.Add(new List<Entry>(currentPage));
+                currentPage.Clear();
+                currentPageIndex++;
+
+                Console.Write($"Strona {currentPageIndex} zapełniona. Czy chcesz kontynuować na kolejnej stronie? (T/N): ");
+                char choice = Console.ReadKey().KeyChar;
+                Console.WriteLine();
+                if (char.ToUpper(choice) == 'N')
+                {
+                    break;
+                }
+            }
+        }
+
+        // Podsumowanie czasu pracy na każdej stronie
+        for (int i = 0; i < pages.Count; i++)
+        {
+            int totalHours = 0;
+            int totalMinutes = 0;
+            foreach (Entry entry in pages[i])
+            {
+                totalHours += entry.Hours;
+                totalMinutes += entry.Minutes;
+            }
+            totalHours += totalMinutes / 60;
+            totalMinutes %= 60;
+
+            Console.WriteLine($"Podsumowanie strona {i + 1}: {totalHours} godzin {totalMinutes} minut.");
+        }
+
+        // Podsumowanie ogólnego czasu pracy
+        int overallHours = 0;
+        int overallMinutes = 0;
+        foreach (var page in pages)
+        {
+            foreach (Entry entry in page)
+            {
+                overallHours += entry.Hours;
+                overallMinutes += entry.Minutes;
+            }
+        }
+        overallHours += overallMinutes / 60;
+        overallMinutes %= 60;
+
+        Console.WriteLine($"Podsumowanie ogólne: {overallHours} godzin {overallMinutes} minut.");
+    }
+}
---
+++ WorkTimeTracking/Program.cs
@@ -0,0 +1,73 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+[TestFixture]
+public class WorkTimeTrackingTests
+{
+    [Test]
+    public void PageSummaryTest()
+    {
+        // Testowanie podsumowania strony
+        List<Entry> page = new List<Entry> { new Entry { Hours = 1, Minutes = 30 }, new Entry { Hours = 2, Minutes = 15 }, new Entry { Hours = 0, Minutes = 45 } };
+        int totalHours, totalMinutes;
+        CalculatePageSummary(page, out totalHours, out totalMinutes);
+        Assert.AreEqual(4, totalHours);
+        Assert.AreEqual(30, totalMinutes);
+    }
+
+    [Test]
+    public void OverallSummaryTest()
+    {
+        // Testowanie ogólnego podsumowania
+        List<List<Entry>> pages = new List<List<Entry>>
+        {
+            new List<Entry> { new Entry { Hours = 1, Minutes = 30 }, new Entry { Hours = 2, Minutes = 15 }, new Entry { Hours = 0, Minutes = 45 } },
+            new List<Entry> { new Entry { Hours = 2, Minutes = 0 }, new Entry { Hours = 1, Minutes = 45 }, new Entry { Hours = 1, Minutes = 0 } }
+        };
+        int overallHours, overallMinutes;
+        CalculateOverallSummary(pages, out overallHours, out overallMinutes);
+        Assert.AreEqual(9, overallHours);
+        Assert.AreEqual(15, overallMinutes);
+    }
+
+    [Test]
+    public void gowno()
+    {
+        Assert.AreEqual(1, 1);
+    }
+
+    // Inne testy można dodać w podobny sposób
+
+    // Dodaj te funkcje, aby testy mogły bezpośrednio korzystać z kodu głównego
+
+    private static void CalculatePageSummary(List<Entry> page, out int totalHours, out int totalMinutes)
+    {
+        totalHours = 0;
+        totalMinutes = 0;
+        foreach (Entry entry in page)
+        {
+            totalHours += entry.Hours;
+            totalMinutes += entry.Minutes;
+        }
+        totalHours += totalMinutes / 60;
+        totalMinutes %= 60;
+    }
+
+    private static void CalculateOverallSummary(List<List<Entry>> pages, out int overallHours, out int overallMinutes)
+    {
+        overallHours = 0;
+        overallMinutes = 0;
+        foreach (var page in pages)
+        {
+            foreach (Entry entry in page)
+            {
+                overallHours += entry.Hours;
+                overallMinutes += entry.Minutes;
+            }
+        }
+        overallHours += overallMinutes / 60;
+        overallMinutes %= 60;
+    }
+}
